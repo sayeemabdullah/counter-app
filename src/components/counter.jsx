ced64047ff4011a58fe7bc2984c7beeb4e20870d
sayeemabdullah <sayeem.mdabdullah@gmail.com>
2021-03-10 13:21:23 +0600

    Added Badge Color Switch

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,11 +14,16 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        {/* <img src={this.state.imgUrl} alt=""></img> */}
-        <span className="badge badge-warning m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count == 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
