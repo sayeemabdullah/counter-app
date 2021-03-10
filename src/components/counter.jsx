2ad1788fe2a065dc695cd532042ee22c23fde473
sayeemabdullah <sayeem.mdabdullah@gmail.com>
2021-03-10 12:32:06 +0600

    Updated counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
   render() {
     return (
-      <React.Fragment>
+      <div>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
-      </React.Fragment>
+      </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count == 0 ? "Zero" : count;
   }
 }
 
