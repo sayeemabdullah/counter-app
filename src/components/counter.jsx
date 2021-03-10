fd16f6108a9e89b6baaf00a0751e60728ffec1af
sayeemabdullah <sayeem.mdabdullah@gmail.com>
2021-03-10 13:10:55 +0600

    Updated counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,20 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    imgUrl: "http://picsum.photos/200",
+  };
+
+  style = {
+    fontSize: 12,
+    fontWeight: "bold",
   };
 
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imgUrl} alt=""></img> */}
+        <span className="badge badge-warning m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
