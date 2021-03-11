8a392bf854eb3d2906c779ec94e17c82cc0e1170
sayeemabdullah <sayeem.mdabdullah@gmail.com>
2021-03-11 12:09:25 +0600

    Added handleIncrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,6 +11,15 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    console.log("Increment Clicked!", this);
+  }
+
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
@@ -26,22 +35,27 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
 
-  // getBadgeClasses() {
-  //   let classes = "badge m-2 badge-";
-  //   classes += this.state.count == 0 ? "warning" : "primary";
-  //   return classes;
-  // }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count == 0 ? "warning" : "primary";
+    return classes;
+  }
 
-  // formatCount() {
-  //   const { count } = this.state;
-  //   return count == 0 ? "Zero" : count;
-  // }
+  formatCount() {
+    const { count } = this.state;
+    return count == 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
