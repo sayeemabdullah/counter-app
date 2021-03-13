8a2e9d4534b4c256094376e0a19e68b2465bb127
sayeemabdullah <sayeem.mdabdullah@gmail.com>
2021-03-13 11:17:21 +0600

    Implementing props

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,9 +4,9 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
-      { id: 3, value: 0 },
+      { id: 3, value: 420 },
       { id: 4, value: 0 },
     ],
   };
@@ -15,7 +15,11 @@ class Counters extends Component {
       <>
         <div>
           {this.state.counters.map((counter) => (
-            <Counter key={counter.id}></Counter>
+            <Counter
+              key={counter.id}
+              value={counter.value}
+              selected={true}
+            ></Counter>
           ))}
         </div>
       </>
