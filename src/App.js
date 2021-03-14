fb9b36b4904df8b628fc871dd4d0fcad8fa72efd
sayeemabdullah <sayeem.mdabdullah@gmail.com>
2021-03-14 17:00:15 +0600

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import React, { Component } from 'react';
-import NavBar from './components/navBar';
-import Counters from './components/counters';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/navBar";
+import Counters from "./components/counters";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
 
-  render() { 
-    return ( <>
-    <NavBar/>
-    <main className="container">
-      <Counters></Counters></main></> );
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counters={this.state.counters}
+          ></Counters>
+        </main>
+      </>
+    );
   }
 }
- 
+
 export default App;
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,9 +4,12 @@ class NavBar extends Component {
   render() {
     return (
       <>
-        <nav class="navbar navbar-light bg-light">
-          <a class="navbar-brand" href="#">
-            Navbar
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              {this.props.totalCounters}
+            </span>
           </a>
         </nav>
       </>
